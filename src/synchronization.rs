c1aa0d256d4e0069f4f992f50f2d1f8fa208ec24
Michal Havlicek <miky.havlicek@gmail.com>
2022-11-22 11:42:50 +0100

    Prevent excessive api calls when using sequential semaphore values.

--- src/synchronization.rs
+++ src/synchronization.rs
@@ -9,51 +9,11 @@ use smallvec::SmallVec;
 
 use crate::{
     arena::{
-        arena::{GenArena, U32Key},
+        arena::{GenArena, Key, U32Key},
         uint::OptionalU32,
     },
     context::device::Device,
 };
-
-// little endian:
-// [value bits, flag bit]
-// big endian:
-// [flag bit, value bits]
-#[derive(Clone, Copy)]
-struct SemaphoreValue(u64);
-
-impl SemaphoreValue {
-    fn new(value: u64, flag: bool) -> Self {
-        assert!(value < 2u64.pow(63));
-
-        #[cfg(target_endian = "little")]
-        return Self(value << 1 | flag as u64);
-        #[cfg(target_endian = "big")]
-        return Self(value >> 1 | flag as u64);
-    }
-    fn value(&self) -> u64 {
-        #[cfg(target_endian = "little")]
-        return self.0 >> 1;
-        #[cfg(target_endian = "big")]
-        return self.0 << 1;
-    }
-    fn set_value(&mut self, value: u64) {
-        assert!(value < 2u64.pow(63));
-
-        self.0 &= 1;
-        #[cfg(target_endian = "little")]
-        let _ = self.0 |= value << 1;
-        #[cfg(target_endian = "big")]
-        let _ = self.0 |= value >> 1;
-    }
-    fn set_flag(&mut self, flag: bool) {
-        self.0 &= !1;
-        self.0 |= flag as u64;
-    }
-    fn flag(&self) -> bool {
-        (self.0 & 1) == 1
-    }
-}
 
 #[derive(Clone)]
 pub struct Queue {
@@ -76,17 +36,19 @@ impl Queue {
 #[derive(Clone, Copy)]
 pub struct Semaphore {
     raw: vk::Semaphore,
-    value: SemaphoreValue,
+    value: u64,
 }
 
 impl Semaphore {
     fn bump_value(&mut self) {
-        self.value.set_value(self.value.value() + 1);
+        self.value += 1;
     }
 }
 
 pub struct QueueSubmitData {
     finished: AtomicBool,
+    is_head: bool,
+    prev: Option<QueueSubmission>,
     queue: Queue,
     semaphore: Semaphore,
 }
@@ -131,7 +93,7 @@ impl InnerSynchronizationManager {
                 let data = data.unwrap();
 
                 let semaphore = data.semaphore.raw;
-                let value = data.semaphore.value.value();
+                let value = data.semaphore.value;
 
                 let info = vk::SemaphoreWaitInfoKHR {
                     flags: vk::SemaphoreWaitFlagsKHR::empty(),
@@ -145,7 +107,23 @@ impl InnerSynchronizationManager {
 
                 match result.raw {
                     vk::Result::SUCCESS => {
-                        data.finished.store(true, Ordering::Relaxed);
+                        let mut data = data;
+                        // we traverse all the previous values and mark them as finished
+                        loop {
+                            let prev = data.finished.fetch_and(true, Ordering::Relaxed);
+                            // the submission has already been waited on and its prev chain has been updated
+                            if prev == true {
+                                break;
+                            }
+                            // visit all the previous values of this semaphore, since this one is finished
+                            // all the previous ones must be too, we update them to prevent
+                            if let Some(key) = data.prev {
+                                data = self.submissions.get(key.0).unwrap();
+                            } else {
+                                break;
+                            }
+                        }
+
                         true
                     }
                     vk::Result::TIMEOUT => {
@@ -179,24 +157,29 @@ impl InnerSynchronizationManager {
         }
     }
     fn allocate(&mut self, queue: Queue, device: &Device) -> QueueSubmission {
-        let semaphore = self.get_fresh_semaphore(device);
-        self.push_submission(queue, semaphore, true)
+        let mut semaphore = self.get_free_semaphore(device);
+        semaphore.bump_value();
+        self.push_submission(queue, semaphore, true, None)
     }
     fn push_submission(
         &mut self,
         queue: Queue,
-        mut semaphore: Semaphore,
-        head: bool,
+        semaphore: Semaphore,
+        is_head: bool,
+        prev: Option<QueueSubmission>,
     ) -> QueueSubmission {
-        semaphore.value.set_flag(head);
         let key = self.submissions.insert(QueueSubmitData {
             finished: AtomicBool::new(false),
+            is_head,
+            prev,
             queue,
             semaphore,
         });
         QueueSubmission(key)
     }
-    fn get_fresh_semaphore(&mut self, device: &Device) -> Semaphore {
+    /// Returns a new semaphore that is free to use, its value field is its current value
+    /// to use it in a wait operation, you need to call bump_value()
+    fn get_free_semaphore(&mut self, device: &Device) -> Semaphore {
         let semaphore = self.free_semaphores.pop().unwrap_or_else(|| {
             let p_next = vk::SemaphoreTypeCreateInfoKHR {
                 semaphore_type: vk::SemaphoreTypeKHR::TIMELINE,
@@ -213,22 +196,26 @@ impl InnerSynchronizationManager {
                     .create_semaphore(&info, device.allocator_callbacks())
                     .unwrap()
             };
-            Semaphore {
-                raw,
-                value: SemaphoreValue::new(0, false),
-            }
+            Semaphore { raw, value: 0 }
         });
-
-        debug_assert!(semaphore.value.flag() == false);
 
         semaphore
     }
     fn collect(&mut self) {
         let drain = self
             .submissions
-            .drain_filter(|s| s.semaphore.value.flag() && s.finished.load(Ordering::Relaxed))
+            .drain_filter(|s| {
+                if s.finished.load(Ordering::Relaxed) {
+                    // we need to make sure to only return the finished heads, because otherwise there will be other timeline values still in submission
+                    if s.is_head {
+                        self.free_semaphores.push(s.semaphore);
+                    }
+                    true
+                } else {
+                    false
+                }
+            })
             .map(|s| s.semaphore);
-        self.free_semaphores.extend(drain);
     }
 }
 
@@ -241,7 +228,9 @@ pub struct AllocateSequential<'a> {
 impl<'a> AllocateSequential<'a> {
     fn alloc(&mut self, queue: Queue) -> QueueSubmission {
         self.semaphore.bump_value();
-        let key = self.manager.push_submission(queue, self.semaphore, false);
+        let key = self
+            .manager
+            .push_submission(queue, self.semaphore, false, self.last);
         self.last = Some(key);
         key
     }
@@ -250,13 +239,7 @@ impl<'a> AllocateSequential<'a> {
 impl<'a> Drop for AllocateSequential<'a> {
     fn drop(&mut self) {
         if let Some(key) = self.last {
-            self.manager
-                .submissions
-                .get_mut(key.0)
-                .unwrap()
-                .semaphore
-                .value
-                .set_flag(true);
+            self.manager.submissions.get_mut(key.0).unwrap().is_head = true;
         } else {
             // no submission was allocated, return the semaphore
             self.manager.free_semaphores.push(self.semaphore.clone())
@@ -281,7 +264,7 @@ impl Device {
     pub fn allocate_sequential(&self) -> AllocateSequential<'_> {
         let mut this: parking_lot::RwLockWriteGuard<'_, _> = self.0.synchronization_manager.write();
         AllocateSequential {
-            semaphore: this.get_fresh_semaphore(self),
+            semaphore: this.get_free_semaphore(self),
             manager: this,
             last: None,
         }
